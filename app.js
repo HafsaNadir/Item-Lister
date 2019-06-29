87c8eeded7d9de0e00c3af1e205da993b69dd266
HafsaNadir <hafsanadir21@gmail.com>
2019-06-29 19:36:48 +0500

    getting input and displaying items

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 let form = document.getElementById('addForm')
 let itemList = document.getElementById('items')
 
-form.addEventListener('submit', addItem)
-
 const addItem = (e) => {
     e.preventDefault()
-    console.log("items")
+    
+    //getting input value
+    let input = document.getElementById("item").value
+    //creating li node
+    let li = document.createElement('li')
+    li.className = "list-group-item"
+    let inputValue = document.createTextNode(input)
+    li.appendChild(inputValue)
+    console.log(li)
+    //appending li node to item list
+    itemList.appendChild(li)
 }
+
+form.addEventListener('submit', addItem)
