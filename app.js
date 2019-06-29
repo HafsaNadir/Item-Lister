a8589849a2b37621a07f316e666c4e2a96f2240a
HafsaNadir <hafsanadir21@gmail.com>
2019-06-29 19:59:34 +0500

    adding delete button

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ const addItem = (e) => {
     e.preventDefault()
     
     //getting input value
-    let input = document.getElementById("item").value
+    let input = document.getElementById('item').value
     //creating li node
     let li = document.createElement('li')
-    li.className = "list-group-item"
+    li.className = 'list-group-item'
     let inputValue = document.createTextNode(input)
     li.appendChild(inputValue)
     console.log(li)
+    //creating delete button
+    let delButton = document.createElement('button')
+    delButton.className = 'btn btn-danger btn-sm float-right delete'
+    delButton.appendChild(document.createTextNode('X')) 
+    //appending delete button to li node
+    li.appendChild(delButton)
     //appending li node to item list
     itemList.appendChild(li)
 }
